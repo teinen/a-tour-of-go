ea23f684ed0ddc55e796f26aef78ad3a43bcc318
諦念 <teinen.github@gmail.com>
2018-08-08 19:30:50 +0900

    Constants

---
+++ basics/constants/constants.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+const Pi = 3.14
+
+func main() {
+	const World = "world"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+
+	// cannot be declared using := syntax
+	// const Cant := false
+}
