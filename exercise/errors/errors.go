36ed85351ee593ad44e775c01eddcbd227357b32
諦念 <teinen.github@gmail.com>
2018-08-12 15:09:42 +0900

    Errors exercise

---
+++ exercise/errors/errors.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+
+	z, t := 1.0, 0.0
+	for i := 0; i < 10; i++ {
+		z, t = z-(z*z-x)/(2*z), z
+		if math.Abs(t-z) < 1e-8 {
+			break
+		}
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
